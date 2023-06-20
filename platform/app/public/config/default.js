1449fcbaa05d1af68ea40e4ae22309caadc496a8
nicholas <nicholas@hippoeducation.com>
2023-06-20 19:05:01 -0400

    updated orthanc config

--- platform/app/public/config/default.js
+++ platform/app/public/config/default.js
@@ -41,18 +41,12 @@ window.config = {
       namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
       sourceName: 'dicomweb',
       configuration: {
-        name: 'aws',
-        // old server
-        // wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
-        // qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-        // wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+        name: 'hippo-orthanc',
+        wadoUriRoot: 'https://ohif.hippoed.com/pacs/wado',
+        qidoRoot: 'https://ohif.hippoed.com/pacs/dicom-web',
+        wadoRoot: 'https://ohif.hippoed.com/pacs/dicom-web',
 
-        // new server
-        wadoUriRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
-        qidoRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
-        wadoRoot: 'https://d33do7qe4w26qo.cloudfront.net/dicomweb',
-
-        qidoSupportsIncludeField: false,
+        qidoSupportsIncludeField: true,
         supportsReject: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
@@ -64,10 +58,6 @@ window.config = {
         // whether the data source should use retrieveBulkData to grab metadata,
         // and in case of relative path, what would it be relative to, options
         // are in the series level or study level (some servers like series some study)
-        bulkDataURI: {
-          enabled: true,
-          relativeResolution: 'studies',
-        },
       },
     },
     {
